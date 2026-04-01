ec73ccb887ad235c111cc1fa5d665ffe3e40ac7b
Thauany da Silva Celestino <2024102060030@aluno.ifro.edu.br>
2026-03-31 20:30:00 -0400

    UpdateMap

--- Profiles/DespesaProfile.cs
+++ Profiles/DespesaProfile.cs
@@ -12,7 +12,13 @@ namespace ApiFinanceiro.Profiles
                 .ForMember(dest => dest.Situacao,
                 opt => opt.MapFrom(scr => "pendente")
                 );
-       
+
+            CreateMap<DespesaUpdateDto, Despesa>()
+                .ForMember(dest => dest.DataPagamento,
+                opt => opt.MapFrom(src => src.DataPagamento.ToDateTime(TimeOnly.FromDateTime(DateTime.Now))
+                )
+                );
+
         }
     }
 }
